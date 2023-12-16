b95e167c972138ee276bd4eaeee68c4ce8224695
P4 <969645+P4@users.noreply.github.com>
2023-12-16 19:49:55 +0100

    feat: implement city card

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,45 @@
+import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardComponent, CardItemDirective } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card #card [list]="cities$ | async" (add)="addCity()">
+      <img src="assets/img/city.png" alt="" width="200" />
+      <app-list-item
+        *appCardItem="let item of card"
+        [id]="item.id"
+        [name]="item.name"
+        (delete)="deleteCity($event)"></app-list-item>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CardItemDirective, AsyncPipe, ListItemComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  protected readonly cities$ = this.store.cities$;
 
-  ngOnInit(): void {}
+  constructor(
+    private http: FakeHttpService,
+    private store: CityStore,
+  ) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((cities) => this.store.addAll(cities));
+  }
+
+  addCity() {
+    this.store.addOne(randomCity());
+  }
+
+  deleteCity(id: number) {
+    this.store.deleteOne(id);
+  }
 }
