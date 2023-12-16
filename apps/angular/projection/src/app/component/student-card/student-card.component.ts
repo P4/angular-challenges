724fa45fec4e5baa201e62b320c374e621306b64
P4 <969645+P4@users.noreply.github.com>
2023-12-16 18:17:20 +0100

    refactor: use content projection for images

--- apps/angular/projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/projection/src/app/component/student-card/student-card.component.ts
@@ -7,10 +7,11 @@ import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-student-card',
-  template: `<app-card
-    [list]="students"
-    [type]="cardType"
-    customClass="bg-light-green"></app-card>`,
+  template: `
+    <app-card [list]="students" [type]="cardType" customClass="bg-light-green">
+      <img src="assets/img/student.webp" width="200px" />
+    </app-card>
+  `,
   standalone: true,
   styles: [
     `
@@ -25,7 +26,10 @@ export class StudentCardComponent implements OnInit {
   students: Student[] = [];
   cardType = CardType.STUDENT;
 
-  constructor(private http: FakeHttpService, private store: StudentStore) {}
+  constructor(
+    private http: FakeHttpService,
+    private store: StudentStore,
+  ) {}
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
